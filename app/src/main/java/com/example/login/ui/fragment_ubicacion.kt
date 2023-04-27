48ca55a0baaf1ed6580b12e99634cbd054f64e25
Yender-M <myenderenrique@gmail.com>
2023-04-26 21:29:59 -0600

    dame amor

--- app/src/main/java/com/example/login/ui/fragment_ubicacion.kt
+++ app/src/main/java/com/example/login/ui/fragment_ubicacion.kt
@@ -5,56 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.Navigation
 import com.example.login.R
+import com.example.login.databinding.FragmentUbicacionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_ubicacion.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_ubicacion : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var fbinding:FragmentUbicacionBinding?=null
+    private  val binding get() = fbinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubicacion, container, false)
+        fbinding = FragmentUbicacionBinding.inflate(inflater, container, false)
+        val view :View = binding.root
+        val img_mapa:ImageView = fbinding!!.imgMapa
+        img_mapa.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.fragment_ubicaciondet2)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragmen_ubicacion.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_ubicacion().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
